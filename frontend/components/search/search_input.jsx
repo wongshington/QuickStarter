cc90bf70a6c6d3b98ff56eebdf6e91ab394139fb
Jesse <jnwong17@gmail.com>
2018-02-14 23:54:20 -0800

    Build search index component

---
+++ frontend/components/search/search_input.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+class SearchIndex extends React.component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+componentDidMount() {
+
+}
+
+componentWillReceiveProps(nextProps) {
+  console.log(nextProps);
+
+}
+
+
+render() {
+
+  return(
+  <div> "this is my searchIndex Component" </div>
+  );
+}
+
+}
