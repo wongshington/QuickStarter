5610fea403c037ec06508fdbb1531bdf6561fa08
Jesse <jnwong17@gmail.com>
2018-02-21 22:32:54 -0800

    render search bar component

---
+++ frontend/components/search/search_index.jsx
@@ -0,0 +1,18 @@
+import React from 'React';
+
+class SearchIndex extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+    <div className= "Searchbar">word up breh</div>
+    );
+  }
+}
+
+export default SearchIndex;
