fb5635bf37cea779a10b44b52a119bdfe5c66f0c
Jesse <jnwong17@gmail.com>
2019-01-16 22:20:40 -0800

    Add general showcase css

--- frontend/components/showcase/showcase_dashboard.jsx
+++ frontend/components/showcase/showcase_dashboard.jsx
@@ -21,18 +21,27 @@ class ShowcaseDashboard extends React.Component {
     return (
       <div to={`/projects/${project.id}`} className="showcase grid">
         <div className="showcase--content-left grid">
+        
           <div></div>
-          <span className="showcase--text showcase--header">Featured Project</span>
+          <div>
+            <span className="showcase--text showcase--header">Featured Project</span>
+          </div>
           <Link to={`/projects/${project.id}`} className="showcase--feature grid">
             <div className="showcase--image showcase--image-main" style={{ backgroundImage: `url(${project.title_image})`, backgroundSize: "cover"} }></div>
             <span className="showcase--text showcase--text-title" >{project.title}</span>
+            <div className="showcase--description"> {/* this section needs to be kept to a certain length otherwise it will mess with the css */}
+              this is some descriptive stuffs?? 
+              thskljbd skbnd;gnsd fngksdf fdgsdfgs dfgsdfg sdgkhgck ghcjfc hgjvhj ;gkfdg bksfdbv sdfkjb sdfkljv sfdnkv dkjvbsdf;k
+            </div>
             <span className="showcase--text showcase--text-author" >By {project.author}</span>
           </Link>
           <div></div>
         </div>
         <div className="showcase--content-right showcase--list grid">
           <div></div>
-          <span className="showcase--header">See More!</span>
+          <div>
+            <span className="showcase--header showcase--header-right">See More!</span>
+          </div>
           <div className="showcase--list-projects grid">
             {items}
           </div>
