5ca9b3b9feb4406fd2326dec7d7762e71238ba49
Jesse <jnwong17@gmail.com>
2019-01-21 16:51:54 -0800

    Add quicksand font

--- frontend/components/search/search_index.jsx
+++ frontend/components/search/search_index.jsx
@@ -4,6 +4,9 @@ class SearchIndex extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      photo: ""
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -13,10 +16,30 @@ class SearchIndex extends React.Component {
     }
   }
 
+  unsplash(){ // this is just to test out some unsplash api business(don't forget ti replace the client_id)
+    console.log("this is inside the unsplash function");
+    let response = fetch("https://api.unsplash.com/photos/random?client_id=CLIENT___ID");
+    response.then( el => el.json() ).then( data => {
+      console.log(data.urls.full, "sfdfdsf");
+      // this.setState({ photo: data.urls.full });
+     }).catch( err => console.log("errrrrr", err));
+    
+  }
+
+  componentDidMount(){
+    // this.unsplash();
+    // uncomment this when the limit is back up
+  }
+
   render() {
     let results = this.props.results;
     if (results[0] === undefined) {
-      return <div>Loading....</div>;
+      return (
+        <div>
+          <img src={`${this.state.photo}`} />
+          Loading....(search_index)
+        </div>
+      );
     } else {
 // by including this classname in this else statement it just wont appear before you search
 
