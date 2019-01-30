44c7f8694a63b2b9d8c898e817c0a7d99ee9de98
Jesse <jnwong17@gmail.com>
2019-01-29 21:25:14 -0800

    Add project index mobile style

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -66,14 +66,7 @@ class navBar extends React.Component {
           <button className="navbar--button button" onClick={() => this.props.history.push("/projects/new")}>Start a Project</button>
         </div>
         <Link className="flex navbar--image"to="/"><img src="http://res.cloudinary.com/quickstarter/image/upload/c_scale,q_100,w_214/v1506394239/quickstarter_logo_b6yrvz.png" /></Link>
-        {/* <div className="flex navbar--section navbar--section-right" >
-          <button className="navbar--button button" onClick={() => console.log("this is search")}>Search</button>
-          <Link to="/login" >Sign In</Link>
-          <Link to="/login" >Guest</Link> */}
           {this.rightNav()}
-          {/* <SearchContainer />
-          <ModalContainer /> */}
-        {/* </div> */}
       </nav>
     );
   }
